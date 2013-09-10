f797a705a5473c1f58c7df40123e6b281bb8f617
Markus Prasser <markus.prasser@uni-jena.de>
2013-09-10 17:10:16 +0200

    Added new private variable 'cumulated_processing_times' to BoundCalc and the SIMPLE U_0 bound from Haouari and Jemmali 2008.

--- bound_calculator.cpp
+++ bound_calculator.cpp
@@ -1,24 +1,36 @@
 #include "bound_calculator.hpp"
 
 BoundCalc::BoundCalc (const Problem &problem):
+	cumulated_processing_times (0),
 	Problem (problem)
 {
-	std::cout << "\nCreating a new BoundCalc instance with the following specifications:\n\tMachines:\t\t" << machines_quantity << "\n\tProcesses:\t\t" << processes_quantity << "\n\tUpper interval bound:\t" << process_interval << "\n\n";
+	for (unsigned short int j = 0; j < processes_quantity; j++) {
+		cumulated_processing_times += processes[j]->get_processing_time ();
+	}
+	std::cout << "\nCreating a new BoundCalc instance with the following specifications:\n\tMachines:\t\t" << machines_quantity << "\n\tProcesses:\t\t" << processes_quantity << "\n\tUpper interval bound:\t" << process_interval << "\n\tCumulated processing times: " << cumulated_processing_times << "\n\n";
+
 }
 
 float BoundCalc::apply_NAIVE_algorithm () {
-	unsigned int cumulated_processing_times = 0;
+	std::cout << "\nApplying NAIVE algorihm\n";
 	unsigned short int longest_processing_time = 0;
-	for (unsigned short int i = 0; i < processes_quantity; i++) {
-		cumulated_processing_times += processes[i]->get_processing_time ();
-		if (processes[i]->get_processing_time () > longest_processing_time)
-			longest_processing_time = processes[i]->get_processing_time ();
+	for (unsigned short int j = 0; j < processes_quantity; j++) {
+		if (processes[j]->get_processing_time () > longest_processing_time)
+			longest_processing_time = processes[j]->get_processing_time ();
 	}
 
 	// Not using the processes_quantity like in the paper, because it doesn't make any sense
 	float average_machine_runtime = cumulated_processing_times / machines_quantity;
 
 	return (average_machine_runtime > longest_processing_time) ? average_machine_runtime : longest_processing_time;
+}
+
+float BoundCalc::apply_SIMPLE_algorithm () {
+	std::cout << "\nApplying SIMPLE algorihm\n";
+
+	float average_machine_runtime = cumulated_processing_times / machines_quantity;
+	
+	return average_machine_runtime;
 }
 
 float BoundCalc::compute_lower_bound (unsigned int algo) {
--- bound_calculator.hpp
+++ bound_calculator.hpp
@@ -7,11 +7,17 @@ class BoundCalc : public Problem {
 		 */
 		BoundCalc (const Problem &problem);
 
-		/*! Applies the NAIVE algorithm to calculate an upper bound
+		/*! Applies the NAIVE algorithm to calculate an upper bound from Ghomi and Ghazvini 1998
 		 * 
 		 * @return The maximum of the cumulated processing times divided by the quantity of machines and the longest process
 		 */
 		float apply_NAIVE_algorithm ();
+
+		/*! Applies the U_0 upper bound from Haouari and Jemmali 2008
+		 *
+		 * @return The average processing time over all machines in the case of perfect balanced distribution
+		 */
+		float apply_SIMPLE_algorithm ();
 
 		/** An alias for 'compute_upper_bounds' for lower bounds
 		 *
@@ -25,4 +31,7 @@ class BoundCalc : public Problem {
 		 * @param invert True, if the lower bound shall be returned
 		 */
 		float compute_upper_bound (unsigned int algo, bool invert = false);
+	
+	private:
+		unsigned int cumulated_processing_times;
 };
