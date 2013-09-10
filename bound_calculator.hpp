26b48fd51478a1111a7272f53d482beca60dff03
Markus Prasser <markus.prasser@uni-jena.de>
2013-09-10 17:18:54 +0200

    Made the algorithm functions of BoundCalc private.

--- bound_calculator.hpp
+++ bound_calculator.hpp
@@ -7,18 +7,6 @@ class BoundCalc : public Problem {
 		 */
 		BoundCalc (const Problem &problem);
 
-		/*! Applies the NAIVE algorithm to calculate an upper bound from Ghomi and Ghazvini 1998
-		 * 
-		 * @return The maximum of the cumulated processing times divided by the quantity of machines and the longest process
-		 */
-		float apply_NAIVE_algorithm ();
-
-		/*! Applies the U_0 upper bound from Haouari and Jemmali 2008
-		 *
-		 * @return The average processing time over all machines in the case of perfect balanced distribution
-		 */
-		float apply_SIMPLE_algorithm ();
-
 		/** An alias for 'compute_upper_bounds' for lower bounds
 		 *
 		 * @param algo The algorithm to use
@@ -33,5 +21,17 @@ class BoundCalc : public Problem {
 		float compute_upper_bound (unsigned int algo, bool invert = false);
 	
 	private:
+		/*! Applies the NAIVE algorithm to calculate an upper bound from Ghomi and Ghazvini 1998
+		 * 
+		 * @return The maximum of the cumulated processing times divided by the quantity of machines and the longest process
+		 */
+		float apply_NAIVE_algorithm ();
+
+		/*! Applies the U_0 upper bound from Haouari and Jemmali 2008
+		 *
+		 * @return The average processing time over all machines in the case of perfect balanced distribution
+		 */
+		float apply_SIMPLE_algorithm ();
+
 		unsigned int cumulated_processing_times;
 };
