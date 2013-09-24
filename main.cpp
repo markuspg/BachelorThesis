347e89d20e770068748b22f110329986d3ed4ee8
Markus Prasser <markus.prasser@uni-jena.de>
2013-09-24 23:42:57 +0200

    Added the ability to write output to a file.

--- main.cpp
+++ main.cpp
@@ -24,12 +24,27 @@ enum START_SOLUTION {iLPT, rLPT, SI, sLPT, STUPID};
 
 int main (int argc, char *argv[]) {
 	std::cout << "<----- PC_min solver ----->\n";
+	std::ofstream output_file_stream;
+	output_file_stream.open ("bound_calculator-results.csv", std::ofstream::out | std::ofstream::trunc);
 
 	for (int t = 1; t < argc; t++) {
+
+		std::string long_filename (argv[t]);
+		std::string short_filename, result;
+		unsigned int found = long_filename.rfind('/');
+		if (found != std::string::npos)
+			short_filename = long_filename.substr (found + 1);
+		else
+			short_filename = long_filename;
+
+		output_file_stream << short_filename << ",";
+
 		// Create a pointer to the problem to be solved
 		Problem *problem = nullptr;
-		problem = new Problem (argv[1]);
+		problem = new Problem (long_filename);
 
 		delete problem;
 	}
+
+	output_file_stream.close ();
 }
