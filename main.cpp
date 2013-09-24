6ad959558a6dc41346ccc36ab324c778009b062d
Markus Prasser <markus.prasser@uni-jena.de>
2013-09-24 10:15:33 +0200

    Made move from 'solver' to 'bound_calculator'.

--- main.cpp
+++ main.cpp
@@ -24,12 +24,45 @@ enum START_SOLUTION {iLPT, rLPT, SI, sLPT, STUPID};
 
 int main (int argc, char *argv[]) {
 	std::cout << "<----- PC_min solver ----->\n";
+	std::ofstream output_file_stream;
+	output_file_stream.open ("bound_calculator-results.csv", std::ofstream::out | std::ofstream::trunc);
 
 	for (int t = 1; t < argc; t++) {
+
+		std::string long_filename (argv[t]);
+		std::string short_filename, result;
+		unsigned int found = long_filename.rfind('/');
+		if (found != std::string::npos)
+			short_filename = long_filename.substr (found + 1);
+		else
+			short_filename = long_filename;
+
+		output_file_stream << short_filename << ",";
+
 		// Create a pointer to the problem to be solved
 		Problem *problem = nullptr;
-		problem = new Problem (argv[1]);
+		problem = new Problem (long_filename);
+
+		BoundCalc *bound_calculator = new BoundCalc (*problem);
+		output_file_stream << std::to_string (bound_calculator->compute_upper_bound (NAIVE)) << ",";
+		delete bound_calculator;
+
+		bound_calculator = new BoundCalc (*problem);
+		output_file_stream << std::to_string (bound_calculator->compute_upper_bound (SIMPLE)) << ",";
+		delete bound_calculator;
+
+		bound_calculator = new BoundCalc (*problem);
+		output_file_stream << std::to_string (bound_calculator->compute_upper_bound (SIMPLE_LINEAR_TIME)) << ",";
+		delete bound_calculator;
+
+		bound_calculator = new BoundCalc (*problem);
+		output_file_stream << std::to_string (bound_calculator->compute_upper_bound (LDM));
+		delete bound_calculator;
+
+		output_file_stream << "\n";
 
 		delete problem;
 	}
+
+	output_file_stream.close ();
 }
