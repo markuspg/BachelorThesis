fefb8bb7c8b1462ee08a83e184a4b55b4add354b
Markus Prasser <markus.prasser@uni-jena.de>
2013-09-25 18:12:48 +0200

    Implemented iterations parameter to cancel infinite iterations.

--- improver.cpp
+++ improver.cpp
@@ -6,7 +6,7 @@ Improver::Improver (const Problem &problem):
 	// std::cout << "\nCreating a new Improver instance with the following specifications:\n\tMachines:\t\t" << machines_quantity << "\n\tProcesses:\t\t" << processes_quantity << "\n\n";
 }
 
-void Improver::apply_PAIRWISE_algorithm (bool greedy) {
+unsigned int Improver::apply_PAIRWISE_algorithm (unsigned int iterations, bool greedy) {
 	// std::cout << "\nApplying pairwise interchange algorihm\n";
 
 	// Compute the naive upper bound (Step 2: Compute lower bound)
@@ -22,7 +22,8 @@ void Improver::apply_PAIRWISE_algorithm (bool greedy) {
 	Machine *pa = nullptr;
 	Machine *pb = nullptr;
 
-	// Create stop criterion for algorithm loop
+	// Create stop criterions for algorithm loop
+	unsigned int iter = 1;
 	bool stop = false;
 	while (!stop) {
 		for (unsigned short int k = 0; k < machines_quantity - 1; k++) {
@@ -41,13 +42,13 @@ void Improver::apply_PAIRWISE_algorithm (bool greedy) {
 			// Stop, if the upper bound correlates to the current solution value (Step 5: If LB = M)
 			if (upper_bound == this->query_lowest_completion_time ()) {
 				// std::cout << "Current schedule correlates to upper bound => Terminating.\n";
-				return;
+				return iter;
 			}
 			
 			// Stop if all machines have the same load (Step 6: Terminate if F_PA = F_PB)
 			if (pa->get_completion_time () == pb->get_completion_time ()) {
 				// std::cout << "Current schedule has all Machines finishing the same time => Terminating.\n";
-				return;
+				return iter;
 			}
 			
 			// Evaluate all advantageous swaps (Step 7: Form sets A and B)
@@ -98,6 +99,7 @@ void Improver::apply_PAIRWISE_algorithm (bool greedy) {
 				set_a.clear ();
 				set_b.clear ();
 			}
+
 		}
 		
 		if (set_a.size () > 0) {
@@ -138,17 +140,22 @@ void Improver::apply_PAIRWISE_algorithm (bool greedy) {
 			// std::cout << "No advantageous swaps can be found anymore, terminating.\n";
 			stop = true;
 		}
+		iter++;
+		if (!greedy) {
+			if (iter >= iterations)
+				stop = true;
+		}
 	}
+
+	return iter;
 }
 
-void Improver::improve_start_solution (unsigned int algo, unsigned short int iterations) {
+unsigned int Improver::improve_start_solution (unsigned int algo, unsigned int iterations) {
 	switch (algo) {
 		case gPAIRWISE:
-			apply_PAIRWISE_algorithm (true);
-			break;
+			return apply_PAIRWISE_algorithm (iterations, true);
 		case PAIRWISE:
-			apply_PAIRWISE_algorithm ();
-			break;
+			return apply_PAIRWISE_algorithm (iterations, false);
 		default:
 			std::cerr << "\nERROR: Invalid improvement algorithm\n";
 	}
--- improver.hpp
+++ improver.hpp
@@ -9,15 +9,12 @@ class Improver : public Problem {
 		Improver (const Problem &problem);
 
 		/*! Applies the pairwise interchange algorithm of Fatemi and Ghazvini (1998) */
-		void apply_gPAIRWISE_algorithm ();
-
-		/*! Applies the pairwise interchange algorithm of Fatemi and Ghazvini (1998) */
-		void apply_PAIRWISE_algorithm (bool greedy = false);
+		unsigned int apply_PAIRWISE_algorithm (unsigned int iterations, bool greedy = false);
 
 		/** Improve a start solution using the given algorithm (PAIRWISE)
 		 *
 		 * @param algo The algorithm to use
 		 * @param iterations The count of iterations the algorithm shall be applied (only affects non-deterministic algorithms)
 		 */
-		void improve_start_solution (unsigned int algo, unsigned short int iterations = 50);
+		unsigned int improve_start_solution (unsigned int algo, unsigned int iterations = 50);
 };
